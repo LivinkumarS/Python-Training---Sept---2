70107949ff69172f3a9c5e0b76bd920691ee9790
LivinkumarS <001liv100@ggmail.com>
2025-09-15 12:24:33 +0530

    Introduction

---
+++ Introduction.py
@@ -0,0 +1,11 @@
+'''the below code is used to print out something.
+the below code is used to print out something.the below code is used to print out something.the below code is used to print out something.the below code is used to print out something.the below code is used to print out something.the below code is used to print out something.the below code is used to print out something.the below code is used to print out something.
+the below code is used to print out something.
+the below code is used to print out something.the below code is used to print out something.the below code is used to print out something.the below code is used to print out something.the below code is used to print out something.the below code is used to print out something.the below code is used to print out something.the below code is used to print out something.the below code is used to print out something.'''
+
+
+print(50)
+print(60)
+
+apple=500
+print(apple)
