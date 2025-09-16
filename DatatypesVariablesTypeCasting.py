138f6a1670c61e82441753e26eb89c59aa5e4196
LivinkumarS <001liv100@ggmail.com>
2025-09-16 12:10:58 +0530

    Datatypes, Type Casting and Variables

---
+++ DatatypesVariablesTypeCasting.py
@@ -0,0 +1,39 @@
+# apple=10
+# Apple=20
+# _apple=30
+# apple1=40
+
+# &apple=30
+# 4apple=10
+
+# actor name 
+
+# actor name=50
+
+# print(apple)
+# print(Apple)
+# print(_apple)
+
+a=34
+b=34.45
+
+c="The Python Training"
+d='123'
+e=123
+
+f=True 
+
+ans=int(d)
+# print(type(ans))
+# print(ans)
+
+name="Ajith kumar"
+age=54
+
+# print(type(f))
+
+print("His name is "+name+". He is "+str(age)+" years old.")
+
+print(f"His name is {name}. He is {age} years old.")
+
+# His name is Ajith kumar. He is 54 years old.
