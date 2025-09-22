e68466fc837c53b5d5273fdf9b864c30db0ddf7f
LivinkumarS <001liv100@ggmail.com>
2025-09-22 12:37:43 +0530

    Loops

---
+++ Loops.py
@@ -0,0 +1,19 @@
+# i=0
+
+# while i>=-50: 
+#     print(i)
+#     i-=1
+
+# str1="Hello world!"
+# print(len(str1))
+
+# for i in range(2,51,2):
+    # print(i)
+    
+    
+for i in range(101):
+    if i%2==0:
+        continue
+    print(i)
+
+print("Loop is executed Successfully!")
