1cec70031220ab0fc4b069cc34e3a5a2b29b587b
LivinkumarS <001liv100@ggmail.com>
2025-09-23 12:29:02 +0530

    Data Structures

---
+++ DataStructures.py
@@ -0,0 +1,75 @@
+# print(lis1[:4])
+# print(lis1[0:7:2])
+
+# str1="Hello world"
+
+# print(str1[::-1])
+
+# lis1[4]="fifty"
+
+# lis1.append("Livinkumar")
+# lis1.append("Vijay")
+
+# lis1.insert(4,"Livinkumar")
+
+# lis1=[1,2,3,4,3,"four",'five','six',True,False,True]
+
+# lis1.remove(3)
+# lis1.pop()
+# lis1.pop(4)
+
+# lis2=[34465,346642,5654,43,65,54,76,24,132,34,54,47,689,56,43]
+
+# print(lis2.index(430))
+# print(lis2.count(43))
+
+# lis2.sort()
+
+# lis2.reverse()
+
+# print(lis2)
+
+# if 0:
+#     print("Hello")
+
+
+tup1=(2,1,3,4,3,5)
+
+# tup1[2]=44
+
+# print(tup1)
+
+# person1 = ("Livinkumar",99,'FED')
+
+# name,age,role=person1
+
+# print(name)
+# print(age)
+# print(role)
+
+# set1={1,2,3,4,5,6}
+# set2={4,5,6,7,8,9,10,11,12}
+
+# print(set2.difference(set1))
+
+
+person1={
+    'name':"Vijay",
+    'age':52,
+    'isMarried':True,
+    1:'one'
+}
+
+person1[1]='Oneeeee!'
+
+
+
+# print(list(person1.keys()))
+# print(person1.values())
+# print(person1.items())
+
+
+
+lis1=[23,12,32,54,34,34,32,54,32,45,2,21,34,43,32]
+
+print(lis1)
