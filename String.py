82b312d74adc0261c5801892cc668a52f6a14520
LivinkumarS <001liv100@ggmail.com>
2025-09-25 12:07:05 +0530

    String

---
+++ String.py
@@ -0,0 +1,20 @@
+
+# ans=str1.upper()
+# ans=ans.lower()
+# ans=str1.title()
+
+# ans=str1.strip()
+
+# ans=str1.startswith("H")
+# ans=str1.endswith("rld")
+# ans=str1.find("lllo")
+# ans=str1.count("llo")
+# ans=str1.replace("Hello","Hi")
+
+str1="Python" #PythonPythonPython
+# lis1=['I', 'would', 'like', 'to', 'code', 'in', 'python']
+
+# ans=str1.split("o")
+
+ans= 'Pyn' not in str1
+print(ans)
